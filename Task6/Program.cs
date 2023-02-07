ca937a768bc71d16b965e4aa33a15a3da9729a1a
AlbulOlya <albul.olya@gmail.com>
2023-02-07 23:21:58 +0500

    Task 6 solved

---
+++ Task6/Program.cs
@@ -0,0 +1,32 @@
+﻿// В Указанном массиве вещественных чисел найдите разницу между максимальным и минимальным элементом
+
+void Print(int[] numbers)
+{
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        Console.Write(numbers[i] + "  ");
+    }
+}
+
+int[] numbers = new int [8];
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    numbers[i] = new Random().Next(-10,11);
+}
+Print(numbers);
+Console.WriteLine();
+int max = numbers[0];
+int min = numbers[0];
+for (int i = 0; i < numbers.Length; i++)
+{
+    if (numbers[i]>max)
+    {
+        max=numbers[i];
+    }
+    else if (numbers[i]<min)
+    {
+        min=numbers[i];
+    }
+}
+Console.WriteLine($"Разница между максимальным и минимальным элементом {max-min}");
